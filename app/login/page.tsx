1a670570be0aa52c9d23695c67a73229b2eb63cf
Jesse Wei <jesseweijapan@gmail.com>
2024-09-01 14:37:04 +0900

    redirect user to correct page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,13 @@
+import { redirect } from "next/navigation"
 import LoginForm from '@/app/ui/form/login-form'
+import { auth } from "@/auth"
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await auth()
+  if (session) {
+    redirect('/app')
+  }
+
   return (
     <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
