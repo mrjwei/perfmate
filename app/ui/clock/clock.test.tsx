26461a0dd9601c6b08b39aed86d576eb995a2e60
Jesse Wei <jesseweijapan@gmail.com>
2024-12-02 18:22:45 +0900

    add test cases for clock component

--- app/ui/clock/clock.test.tsx
+++ app/ui/clock/clock.test.tsx
@@ -1,24 +1,51 @@
 import React from 'react'
 import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest'
-import { act, render, screen, cleanup } from '@testing-library/react'
-import Clock from './clock'
+import { render, screen, cleanup, act } from '@testing-library/react'
+import Clock from './Clock'
 
 describe('Clock', () => {
-  beforeEach(async () => {
-    vi.useFakeTimers()
-    const date = new Date('2024-07-27T01:02:03')
-    vi.setSystemTime(date)
-    render(<Clock initialTime={date} />)
-  })
+  let mockDate = new Date('2024-07-27T01:02:03')
+
   afterEach(() => {
-    cleanup()
+    vi.setSystemTime(Date.now())
     vi.useRealTimers()
+    mockDate = new Date('2024-07-27T01:02:03')
+    cleanup()
   })
+
   it('renders initial time correctly', () => {
+    vi.setSystemTime(mockDate)
+    render(<Clock />)
     expect(screen.getByText('01:02:03')).toBeInTheDocument()
+    expect(screen.getByText('2024-07-27 (Sat)')).toBeInTheDocument()
   })
+
   it('updates time every second', async () => {
-    act(() => vi.advanceTimersByTime(1000))
+    vi.useFakeTimers()
+    vi.setSystemTime(mockDate)
+    render(<Clock />)
+    expect(screen.getByText('01:02:03')).toBeInTheDocument()
+    act(() => {
+      vi.advanceTimersByTime(1000);
+    });
     expect(screen.getByText('01:02:04')).toBeInTheDocument()
+    act(() => {
+      vi.advanceTimersByTime(1000);
+    });
+    expect(screen.getByText('01:02:05')).toBeInTheDocument()
+  })
+
+  it('updates date string correctly', async () => {
+    mockDate = new Date('2024-07-27T23:59:59')
+    vi.useFakeTimers()
+    vi.setSystemTime(mockDate)
+    render(<Clock />)
+    expect(screen.getByText('23:59:59')).toBeInTheDocument()
+    expect(screen.getByText('2024-07-27 (Sat)')).toBeInTheDocument()
+    act(() => {
+      vi.advanceTimersByTime(1000);
+    });
+    expect(screen.getByText('24:00:00')).toBeInTheDocument()
+    expect(screen.getByText('2024-07-28 (Sun)')).toBeInTheDocument()
   })
 })
